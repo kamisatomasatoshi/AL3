0150befa973c446e429737bcba09bc2676d49dea
kamisatomasatoshi <k021g1271@g.neec.ac.jp>
2022-04-14 15:03:36 +0900

    確認課題１－２

--- scene/GameScene.cpp
+++ scene/GameScene.cpp
@@ -6,7 +6,11 @@ using namespace DirectX;
 
 GameScene::GameScene() {}
 
-GameScene::~GameScene() {}
+GameScene::~GameScene() 
+{ 
+	delete sprite_; 
+	delete model_;
+}
 
 void GameScene::Initialize() {
 
@@ -14,9 +18,57 @@ void GameScene::Initialize() {
 	input_ = Input::GetInstance();
 	audio_ = Audio::GetInstance();
 	debugText_ = DebugText::GetInstance();
+	textureHandle_ = TextureManager::Load("uvChecker.png");
+	sprite_ = Sprite::Create(textureHandle_, {100, 50});
+	model_ = Model::Create();
+	//ワールドドランスフォーム初期化
+	worldTransform_.Initialize();
+	//ビュープロダクションの初期化
+	viewProjection_.Initialize();
+	//サウンドデータの読み込み
+	soundDataHandle_ = audio_->LoadWave("fanfare.wav");
+	//サウンドの再生
+	
+	//audio_->PlayWave(soundDataHandle_);
+	//音声再生
+	voiceHandle_ = audio_->PlayWave(soundDataHandle_, true);
+
+	
 }
 
-void GameScene::Update() {}
+void GameScene::Update() 
+{
+	XMFLOAT2 position = sprite_->GetPosition();
+
+	//座標を｛２，０｝移動
+	position.x += 2.0f;
+	position.y += 1.0f;
+
+	//移動した座標をスプライトに反映
+	sprite_->SetPosition(position);
+	if (input_->TriggerKey(DIK_SPACE)) {
+		//再生停止
+		audio_->StopWave(soundDataHandle_);
+	}
+
+	//デバックテキスト表示]
+	//debugText_->Printf("kaizokuou ni oreha naru. ", 50, 50, 1.0f);
+
+	//書式指定付き表示
+	debugText_->SetPos(50, 70);
+	debugText_->Printf("year:%d", 2001);
+
+	//変数の値をインクリメント
+	value_++;
+	// 値を含んだ文字列
+	std::string strDebug = std::string("Value") + 
+	std::to_string(value_);
+
+	//デバックテキストの表示
+	debugText_->Print(strDebug, 50, 50, 1.0f);
+
+
+}
 
 void GameScene::Draw() {
 
@@ -44,7 +96,7 @@ void GameScene::Draw() {
 	/// <summary>
 	/// ここに3Dオブジェクトの描画処理を追加できる
 	/// </summary>
-
+	model_->Draw(worldTransform_, viewProjection_, textureHandle_);
 	// 3Dオブジェクト描画後処理
 	Model::PostDraw();
 #pragma endregion
@@ -56,7 +108,7 @@ void GameScene::Draw() {
 	/// <summary>
 	/// ここに前景スプライトの描画処理を追加できる
 	/// </summary>
-
+	sprite_->Draw();
 	// デバッグテキストの描画
 	debugText_->DrawAll(commandList);
 	//
